2728f2752304dc5efffb386a442604a0f1968e81
Senta Fouché <64000466+sfouche55@users.noreply.github.com>
2021-03-30 11:28:01 +0200

    Correct tslint warnings.

--- Frontend/PhoneBook/src/app/components/manage-contact/manage-contact.component.spec.ts
+++ Frontend/PhoneBook/src/app/components/manage-contact/manage-contact.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ManageContactComponent } from './manage-contact.component';
 
@@ -6,7 +6,7 @@ describe('ManageContactComponent', () => {
   let component: ManageContactComponent;
   let fixture: ComponentFixture<ManageContactComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ManageContactComponent ]
     })
--- Frontend/PhoneBook/src/app/components/view-contacts/view-contacts.component.spec.ts
+++ Frontend/PhoneBook/src/app/components/view-contacts/view-contacts.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ViewContactsComponent } from './view-contacts.component';
 
@@ -6,7 +6,7 @@ describe('ViewContactsComponent', () => {
   let component: ViewContactsComponent;
   let fixture: ComponentFixture<ViewContactsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ViewContactsComponent ]
     })
